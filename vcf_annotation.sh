a91c1868693eb115ec33e3fd1746cee8dd7721cd
Kim McLaughlin <kimmclaughlin@Kims-MacBook-Air.local>
2018-08-13 16:11:17 -0400

    updated file

--- vcf_annotation.sh
+++ vcf_annotation.sh
@@ -18,7 +18,7 @@ fi
 ./annotate_variation.pl -downdb -webfrom annovar -build hg19 refGene humandb/
 #Create annotated vcf with ExAC allele frequencies and variant types
 ./table_annovar.pl annovar.avinput humandb/ -buildver hg19 -out annotation -protocol refGene,exac03 -operation g,f
-#Extract Read Depth, Number of Reads Supporting the Variant, and Percentage of Reads supporting the Variant vs Supporting Reference Reads from ANNOVAR input file
+#Extract Read Depth, Number of Reads Supporting the Variant, Percentage of Reads supporting the Variant vs Supporting Reference Reads from ANNOVAR input file, and Alternate Allele Frequency (VCF information)
 cut -f 13 annovar.avinput | awk -F \; '{print $8, $33, $30, $10, $4}' | awk '{gsub(/[^0-9. ]/,"")}1' | awk '{print $1 "\t" $2 + $3 "\t" $4 "\t" $5}' > vcf_information.txt
 #Create header for VCF information file
 echo -e 'Read Depth\tNumber of Reads Supporting the Variant\tPercentage of Reads Supporting the Variant vs Supporting Reference Reads\tAlternate Allele Frequency' > header.txt
@@ -27,7 +27,7 @@ cat header.txt vcf_information.txt > exac_header.txt
 #combine VCF information with ANNOVAR output 
 paste annotation.hg19_multianno.txt exac_header.txt > final_${vcf}.txt
 
-#remove unneeded files
+#remove intermediate files
 rm exac_header.txt
 rm vcf_information.txt
 rm header.txt
